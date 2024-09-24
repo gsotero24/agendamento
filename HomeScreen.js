85b34a03496d6aa613bae2ef767b5047dc1af945
gsotero24 <grcgiovannadebora@gmail.com>
2024-09-23 21:52:04 -0300

    Create HomeScreen.js
    Exibir um botão que levará o usuário para página de agendamento.

---
+++ HomeScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo ao Salão de Beleza</Text>
+      <Button
+        title="Agendar um Horário"
+        onPress={() => navigation.navigate('Schedule')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+});
